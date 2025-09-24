7a1a0bc97e11db43deb732aac97971bdac6e324a
IT24103498 <IT24103498@my.sliit.lk>
2025-09-24 20:47:35 +0530

    Lab_06

---
+++ Lab Sheet 06/IT24103498.R
@@ -0,0 +1,13 @@
+setwd("D:\\SLIIT\\Y2.S1\\PS")
+
+#Question 01
+
+#i. What is the distribution of X?
+#Binomial Distribution
+#here,random variable x has binomial distribution with n=50 and p=0.85
+
+#ii. What is the probability that at least 47 students passed the test?
+pbinom(46,50,0.85)
+
+#Question 02
+
